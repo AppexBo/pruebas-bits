5551ee4210d0266855a12e798f78dd4dcf21e5b3
Jhenyfer <leonardo.eguez@appexbo.com>
2025-05-04 22:32:51 -0400

    ultimo dev en produccion de bits and cream

---
+++ hide_2_col_in_stock_quantity/__manifest__.py
@@ -0,0 +1,21 @@
+{
+	"name" : "Modulo para ocultar las 2 columnas a usuarios en especifico",
+	"version" : "17.0.0.0",
+	"category" : "Warehouse",
+	"depends" : [
+		'base',
+		'stock',
+	],
+	"author": "AppexBo",
+	'summary': 'Ocultar 2 columans para 2 usuarios especificos',
+	"description": "Ocultar 2 columans para 2 usuarios especificos",
+	"website" : "https://www.appexbo.com/",
+	
+	"auto_install": False,
+	"installable": True,
+	
+	"license": "LGPL-3",
+	"data": [
+		'views/stock_quant_list_views.xml'
+	],
+}
